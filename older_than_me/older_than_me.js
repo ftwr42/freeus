5e7e7ce3069e1da3c08a591e1329776739b6331e
painerp <8081128+painerp@users.noreply.github.com>
2023-11-20 17:42:17 +0100

    fill compareAge function to Person class

--- older_than_me/older_than_me.js
+++ older_than_me/older_than_me.js
@@ -5,5 +5,13 @@ class Person {
         this.age = age;
     }
 
-    function compareAge{} 
+    compareAge(other) {
+        if (this.age < other.age) {
+            return `${other.name} is older than me.`;
+        } else if (this.age > other.age) {
+            return `${other.name} is younger than me.`;
+        } else {
+            return `${other.name} is the same age as me.`;
+        }
+    }
 }
