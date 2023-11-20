84f0e5ccb4ae1f199e9b4f66535eab890bd66056
raumohnewand <47612720+raumohnewand@users.noreply.github.com>
2023-11-20 17:41:53 +0100

    Update main.js

--- older_than_me/main.js
+++ older_than_me/main.js
@@ -3,3 +3,9 @@ import { Person } from './older_than_me.js';
 p1 = Person("Samuel", 24)
 p2 = Person("Joel", 36)
 p3 = Person("Lily", 24)
+
+p1.compareAge(p2) 
+
+p2.compareAge(p1) 
+
+p1.compareAge(p3)
